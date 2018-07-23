f6db1680477a577a4af2e8602f0e8bf66381369e
CurlyBraceTT <ucan.not.advance@gmail.com>
2018-07-23 13:45:30 +0300

    Minor Clean up

--- Services/RedditApiService.cs
+++ Services/RedditApiService.cs
@@ -1,14 +1,11 @@
 ﻿using System.Collections.Generic;
-using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.WebUtilities;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using RedditLiveFeed.Model;
 using RedditLiveFeed.Services.Interfaces;
-using RedditLiveFeed.Utils;
 using RedditLiveFeed.Utils.Interfaces;
 
 namespace RedditLiveFeed.Services
